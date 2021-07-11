caf0cf4db4bdd53a01595f2772a1f03713be3cac
WangGossip <982728182@qq.com>
2021-07-11 14:38:02 +0800

    7.11关于VGG的修改

--- model.py
+++ model.py
@@ -2,13 +2,15 @@
 #*  这个文件用于存放基本的模型
 
 import torch
-from torch._C import namedtuple_a_tau
 import torch.nn as nn
 import torch.nn.functional as F
+from torchsummary import summary
 
 def get_model(model_name):
     if model_name == 'Net1':
         return Net1
+    elif model_name == 'VGG16':
+        return VGG([2,2,3,3,3], num_classes=10)
 
 # *这个网络目前还不知道是叫什么结构，现没有名字
 class Net1(nn.Module):
@@ -35,4 +37,55 @@ class Net1(nn.Module):
         x = self.fc2(x)
         return x, e1
 
+class VGG(nn.Module):
+    """
+    VGG builder
+    """
+    def __init__(self, arch: object, num_classes=10) -> object:
+        super(VGG, self).__init__()
+        # - 输入的通道数（用于两个MNIST数据集）
+        self.in_channels = 1
+        self.conv3_64 = self.__make_layer(64, arch[0])
+        self.conv3_128 = self.__make_layer(128, arch[1])
+        self.conv3_256 = self.__make_layer(256, arch[2])
+        self.conv3_512a = self.__make_layer(512, arch[3])
+        self.conv3_512b = self.__make_layer(512, arch[4])
+        
+        self.fc1 = nn.Linear(7*7*512, 4096)
+        self.bn1 = nn.BatchNorm1d(4096)
+        self.bn2 = nn.BatchNorm1d(4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, num_classes)
+
+    def __make_layer(self, channels, num):
+        layers = []
+        for i in range(num):
+            layers.append(nn.Conv2d(self.in_channels, channels, kernel_size=3, stride=1, padding=1, bias=False))  # same padding
+            layers.append(nn.BatchNorm2d(channels))
+            layers.append(nn.ReLU())
+            self.in_channels = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv3_64(x)
+        out = F.max_pool2d(out, 2)
+        out = self.conv3_128(out)
+        out = F.max_pool2d(out, 2)
+        out = self.conv3_256(out)
+        out = F.max_pool2d(out, 2)
+        out = self.conv3_512a(out)
+        out = F.max_pool2d(out, 2)
+        out = self.conv3_512b(out)
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.fc2(out)
+        out = self.bn2(out)
+        out = F.relu(out)
+        e1 = out
+        out = self.fc3(out)
+        out = F.softmax(out)
+        return out, e1
 
--- run.py
+++ run.py
@@ -1,29 +1,78 @@
 import numpy as np
 import time
 import torch
+import torch.optim as optim
+import torch.nn.functional as F
+
 from torch.utils.data.dataloader import DataLoader
 from torchvision import datasets, transforms
 from torch.utils import data
+from torch.optim.lr_scheduler import StepLR
 
 # 个人文件函数等
 from functions import csv_test, Timer, Accumulator, show_images, get_fashion_mnist_labels
 from datasets import get_dataset, get_handler
+from model import get_model
 import arguments
 
+from d2l import torch as d2l
 # *关于这个baseline的函数功能：
 # 1. 对于不同数据集开展实验
 # 2. 尝试使用不同的网络模型
 # 3. 测试一下各种AL采集函数
 # 4. 运行结果保存为基线，画图形式即可
+
+#* 单个迭代过程训练函数
+def train_epoch(args, model, device, train_loader, optimizer, epoch, Timer):
+    model.train()
+    for batch_idx, (data, target, idxs) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output, e1 = model(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        # - 记录结果到csv中，并输出到日志（控制台即可）
+        if batch_idx % args.log_interval == 0:
+            tmp_time = Timer.stop()
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}, time is {}s'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item(), tmp_time))
+            # -存储到csv中
+            if args.dry_run:
+                break
+# *测试函数
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target, idxs in test_loader:
+            data, target = data.to(device), target.to(device)
+            output, e1 = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
 def main(args):
+
     # **测试函数部分
+    d2l.load_data_fashion_mnist
     # T = Timer()
     # data_train, labels_train, _, _ = get_dataset('FashionMNIST', './datasets') 
     # X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
     # show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y));
     # total_time = T.stop()
     # ** 测试结束
-
+    # *参数人为赋值
+    args.model_name = 'VGG16'
     # 正式的训练过程
     T = Timer()
     # * 参数处理部分
@@ -34,7 +83,7 @@ def main(args):
                             transforms.Normalize((0.1307,), (0.3081,))])},
                     'FashionMNIST':
                         {'transform':transforms.Compose([transforms.ToTensor(), 
-                            transforms.Normalize((0.1307,), (0.3081,))])},
+                            transforms.Normalize((0.1307,), (0.3081,)),transforms.Resize(224)])},
                     'SVHN':
                         {'transform':transforms.Compose([transforms.ToTensor(), 
                             transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970))])},
@@ -44,12 +93,12 @@ def main(args):
     }
     # 是否使用GPU
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-    use_cuda = False
     device = torch.device("cuda" if use_cuda else "cpu")
 
     # *加载数据集，设置dataloader等
     # 数据集相关参数
     transform = transform_pool[DATA_NAME]['transform']
+    
     train_kwargs = {'batch_size': args.batch_size}
     test_kwargs = {'batch_size': args.test_batch_size}
     if use_cuda:
@@ -73,11 +122,18 @@ def main(args):
     print('Read Data time is {} s'.format(tmp_t))
 
     # *模型训练部分
-    # 加载、选择模型
+    # 加载、选择模型，设置优化器、处理相关参数
+    net = get_model(args.model_name).to(device)
+    optimizer = optim.Adadelta(net.parameters(), lr=args.lr)
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    n_epoch = args.epochs
 
-
+    for epoch in range(1, n_epoch+1):
+        train_epoch(args, net, device, train_loader, optimizer, n_epoch, T)
+        test(net, device, test_loader)
+        scheduler.step()
     tmp_t = T.stop()
-    print('Draw Data time is {} s'.format(tmp_t))
+    print('Total Data time is {} s'.format(tmp_t))
 if __name__ == '__main__' :
     args = arguments.get_args()
     main(args)
